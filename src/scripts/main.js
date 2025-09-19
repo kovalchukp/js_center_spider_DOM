22b5ef50d779a1a7adede477d1cf2b27439ca96e
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-09-18 22:42:31 -0600

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const leftPosition = (wall.offsetWidth - spider.offsetWidth) / 2;
+
+  const topPosition = (wall.offsetHeight - spider.offsetHeight) / 2;
+
+  spider.style.left = `${leftPosition}px`;
+  spider.style.top = `${topPosition}px`;
+});
