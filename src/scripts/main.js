7addc5c2fe2e02750b69616de9310f5b9d567136
kovalchukp <you@example.comp.kovalchuk@icloud.com>
2025-09-18 22:54:26 -0600

    fix center position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
 
-  const leftPosition = (wall.offsetWidth - spider.offsetWidth) / 2;
+  const leftPosition = (wall.clientWidth - spider.offsetWidth) / 2;
 
-  const topPosition = (wall.offsetHeight - spider.offsetHeight) / 2;
+  const topPosition = (wall.clientHeight - spider.offsetHeight) / 2;
 
   spider.style.left = `${leftPosition}px`;
   spider.style.top = `${topPosition}px`;
